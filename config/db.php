b388dc36053c3e771919eeed4cef29f7831514c4
tudor.pascu <tudor.pascu@booking.com>
2026-06-08 15:23:16 +0300

    mysql and redis host update

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1;dbname=game',
+    'dsn' => 'mysql:host=mysql-service;dbname=game',
     'username' => 'root',
     'password' => 'Hamham1miau!',
     'charset' => 'utf8',
--- config/redis.php
+++ config/redis.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\redis\Connection',
-    'hostname' => getenv('REDIS_HOST') ?: 'localhost',
+    'hostname' => getenv('REDIS_HOST') ?: 'redis-service',
     'port' => (int)(getenv('REDIS_PORT') ?: 6379),
     'database' => (int)(getenv('REDIS_DATABASE') ?: 0),
     'password' => getenv('REDIS_PASSWORD') ?: null,
