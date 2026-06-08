5febc7cc1d0189f0ad07668930b64b206af00e80
tudor.pascu <tudor.pascu@booking.com>
2026-06-08 14:35:23 +0300

    pod force delete and prod env

--- web/index.php
+++ web/index.php
@@ -2,11 +2,12 @@
 
 // comment out the following two lines when deployed to production
 // in index.php or your entry script
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+//error_reporting(E_ALL);
+//ini_set('display_errors', 1);
 
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
+// Update these lines at the top of the file to trust system environment variables
+defined('YII_DEBUG') or define('YII_DEBUG', isset($_ENV['YII_DEBUG']) ? (bool)$_ENV['YII_DEBUG'] : false);
+defined('YII_ENV') or define('YII_ENV', isset($_ENV['YII_ENV']) ? $_ENV['YII_ENV'] : 'prod');
 
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
