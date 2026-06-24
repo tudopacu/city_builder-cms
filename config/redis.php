27e7acb08a985babeb58a3cd0a116a8bd422455e
tudor.pascu <tudor.pascu@booking.com>
2026-06-24 16:02:26 +0300

    force redis pass to null if missing

--- config/redis.php
+++ config/redis.php
@@ -5,5 +5,5 @@ return [
     'hostname' => getenv('REDIS_HOST'),
     'port' => (int)(getenv('REDIS_PORT')),
     'database' => (int)(getenv('REDIS_DATABASE')),
-    'password' => getenv('REDIS_PASSWORD'),
+    'password' => getenv('REDIS_PASSWORD') ?: null,
 ];
