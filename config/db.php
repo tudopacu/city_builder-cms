b4efc1ecb3971dd1a4e63e0893ad58152dde74fa
tudor.pascu <tudor.pascu@booking.com>
2026-06-24 14:09:56 +0300

    credentials to env

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=mysql-service;dbname=game',
-    'username' => 'root',
-    'password' => 'Hamham1miau!',
+    'dsn' => 'mysql:host=' . getenv('DB_HOST') . ';dbname=' . getenv('DB_NAME'),
+    'username' => getenv('DB_USER'),
+    'password' => getenv('DB_PASSWORD'),
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
--- config/redis.php
+++ config/redis.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\redis\Connection',
-    'hostname' => getenv('REDIS_HOST') ?: 'redis-service',
-    'port' => (int)(getenv('REDIS_PORT') ?: 6379),
-    'database' => (int)(getenv('REDIS_DATABASE') ?: 0),
-    'password' => getenv('REDIS_PASSWORD') ?: null,
+    'hostname' => getenv('REDIS_HOST'),
+    'port' => (int)(getenv('REDIS_PORT')),
+    'database' => (int)(getenv('REDIS_DATABASE')),
+    'password' => getenv('REDIS_PASSWORD'),
 ];
