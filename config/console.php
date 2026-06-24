55777ff268eafb6175322b482f094885635cb67c
tudor.pascu <tudor.pascu@booking.com>
2026-06-24 15:28:02 +0300

    gii load only if vendor is present

--- config/console.php
+++ config/console.php
@@ -38,19 +38,20 @@ $config = [
 ];
 
 if (YII_ENV_DEV) {
-    // configuration adjustments for 'dev' environment
-    $config['bootstrap'][] = 'gii';
-    $config['modules']['gii'] = [
-        'class' => 'yii\gii\Module',
-    ];
-    // configuration adjustments for 'dev' environment
-    // requires version `2.1.21` of yii2-debug module
-    $config['bootstrap'][] = 'debug';
-    $config['modules']['debug'] = [
-        'class' => 'yii\debug\Module',
-        // uncomment the following to add your IP if you are not connecting from localhost.
-        //'allowedIPs' => ['127.0.0.1', '::1'],
-    ];
+    // Check if the debug module was actually installed by composer
+    if (class_exists('yii\debug\Module')) {
+        $config['bootstrap'][] = 'debug';
+        $config['modules']['debug'] = [
+            'class' => 'yii\debug\Module',
+        ];
+    }
+
+    if (class_exists('yii\gii\Module')) {
+        $config['bootstrap'][] = 'gii';
+        $config['modules']['gii'] = [
+            'class' => 'yii\gii\Module',
+        ];
+    }
 }
 
 return $config;
