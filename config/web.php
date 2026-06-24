0b4dbcbe21bfc5c85933313e3dc063d05c4fe15e
tudor.pascu <tudor.pascu@booking.com>
2026-06-24 15:43:34 +0300

    gii load only if vendor is present

--- config/web.php
+++ config/web.php
@@ -88,20 +88,22 @@ $config = [
 ];
 
 if (YII_ENV_DEV) {
-    // configuration adjustments for 'dev' environment
-    $config['bootstrap'][] = 'debug';
-    $config['modules']['debug'] = [
-        'class' => 'yii\debug\Module',
-        // uncomment the following to add your IP if you are not connecting from localhost.
-        //'allowedIPs' => ['127.0.0.1', '::1'],
-    ];
+    // Only bootstrap and configure if the package exists
+    if (class_exists('yii\debug\Module')) {
+        $config['bootstrap'][] = 'debug';
+        $config['modules']['debug'] = [
+            'class' => 'yii\debug\Module',
+            'allowedIPs' => ['127.0.0.1', '::1', '10.*', '192.168.*'],
+        ];
+    }
 
-    $config['bootstrap'][] = 'gii';
-    $config['modules']['gii'] = [
-        'class' => 'yii\gii\Module',
-        // uncomment the following to add your IP if you are not connecting from localhost.
-        //'allowedIPs' => ['127.0.0.1', '::1'],
-    ];
+    if (class_exists('yii\gii\Module')) {
+        $config['bootstrap'][] = 'gii';
+        $config['modules']['gii'] = [
+            'class' => 'yii\gii\Module',
+            'allowedIPs' => ['127.0.0.1', '::1', '10.*', '192.168.*'],
+        ];
+    }
 }
 
 return $config;
