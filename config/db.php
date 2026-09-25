a5fe94ae676188beea7d4f5a2e28dfe72c2bfe4f
Tudor Pascu <tudor.pascu@booking.com>
2026-09-25 15:18:06 +0300

    db config update

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=' . getenv('DB_HOST') . ';dbname=' . getenv('DB_NAME'),
+    'dsn' => 'mysql:host=' . getenv('DB_HOST'). '.default.svc.cluster.local' . ';dbname=' . getenv('DB_NAME'),
     'username' => getenv('DB_USER'),
     'password' => getenv('DB_PASSWORD'),
     'charset' => 'utf8',
