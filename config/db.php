447defa92ff3a908290dca64ae506f7241584aa5
Tudor Pascu <tudor.pascu@booking.com>
2026-09-25 16:02:32 +0300

    localhost test

--- config/db.php
+++ config/db.php
@@ -2,7 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=' . getenv('DB_HOST'). ';dbname=' . getenv('DB_NAME'),
+//    'dsn' => 'mysql:host=' . getenv('DB_HOST'). ';dbname=' . getenv('DB_NAME'),
+    'dsn' => 'mysql:host=localhost;dbname=' . getenv('DB_NAME'),
     'username' => getenv('DB_USER'),
     'password' => getenv('DB_PASSWORD'),
     'charset' => 'utf8',
